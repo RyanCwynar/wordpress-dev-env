56146b1cb82f6a3758664472909d8e2fa0653d28
Ryan Cwynar <rtcx86@gmail.com>
2020-02-24 19:33:27 -0500

    upgrade mysql

--- sync-wpengine.sh
+++ sync-wpengine.sh
@@ -1,3 +1,3 @@
-rsync -a $1@$1.ssh.wpengine.net:/sites/$1/wp-content/uploads ./wordpress/wp-content
-rsync -a $1@$1.ssh.wpengine.net:/sites/$1/wp-content/plugins ./wordpress/wp-content 
-scp $1@$1.ssh.wpengine.net:/sites/$1/wp-content/mysql.sql ./sql/mysql.sql 
+rsync -aP $1@$1.ssh.wpengine.net:/sites/$1/wp-content/uploads ./wordpress/wp-content
+rsync -aP $1@$1.ssh.wpengine.net:/sites/$1/wp-content/plugins ./wordpress/wp-content 
+rsync -aP $1@$1.ssh.wpengine.net:/sites/$1/wp-content/mysql.sql ./sql/mysql.sql 
